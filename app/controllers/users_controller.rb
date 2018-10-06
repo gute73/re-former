d2428db99b5b3520926fecde9aca1d56d8779b3b
Mark D. King <kingmarkd@gmail.com>
2018-10-06 16:41:48 -0400

    Completed: Create New Users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+	include UsersHelper
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		redirect_to new_user_path
+  	else
+  		render :new
+  	end
+  end
+
+end
